53ac3dbfedfbf2ec46654ca8792df46ea8117947
kourov1024 <kourov1024@gmail.com>
2022-05-18 10:32:32 +0300

    Добавьте файлы проекта.

---
+++ LPR0_Derbin/MainWindow.xaml.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace LPR0_Derbin
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        int num = 0;
+        public List<Point> points = new List<Point>();
+        public List<Otrezki> otrezki = new List<Otrezki>();
+        string[] ABC = new string[6] { "A", "B", "C", "D", "E"," "};
+        public MainWindow()
+        {
+            InitializeComponent();
+            numTextBlock.Text = "Точка " + ABC[num];
+        }
+
+        private void NextKoordinate(object sender, RoutedEventArgs e)
+        {
+            num++;
+            numTextBlock.Text = "Точка " + ABC[num];
+            float X = ProverkaPole(textBoxKoor1.Text);
+            float Y = ProverkaPole(textBoxKoor2.Text);
+            float Z = ProverkaPole(textBoxKoor3.Text);
+            ClearPole();
+            points.Add(new Point(X, Y, Z));
+            if (num != 5) return;
+            
+            Result result = new Result(GetAllOtrez());
+            this.Close();
+            result.Show();
+
+        }
+        public float ProverkaPole(string s)
+        {
+            if (s=="")
+            {
+                return 0;
+            }
+            else
+            {
+                return Convert.ToSingle(s);
+            }
+        }
+        private void ClearPole()
+        {
+            textBoxKoor1.Text = " ";
+            textBoxKoor2.Text = " ";
+            textBoxKoor3.Text = " ";
+
+        }
+
+        private List<Otrezki> GetAllOtrez()
+        {
+            int n = 0;
+            for (int i = 0; i < points.Count(); i++)
+            {
+                for (int j = i+1; j < points.Count(); j++)
+                {
+                    n++;
+                    //MessageBox.Show(points[j].X.ToString()+" "+ points[j].X.ToString() + " " + points[j].X.ToString());
+                    double distance = Math.Sqrt(((points[i].X - points[j].X) * (points[i].X - points[j].X)) + ((points[i].Y - points[j].Y) * (points[i].Y - points[j].Y)) + ((points[i].Z - points[j].Z) * (points[i].Z - points[j].Z)));
+                    otrezki.Add(new Otrezki(ABC[i], ABC[j], Math.Round(distance, 3)));
+                }
+            }
+            return otrezki;
+        }
+
+    }
+
+    public class Point
+    {
+        public float X;
+        public float Y;
+        public float Z;
+
+        public Point(float x, float y, float z)
+        {
+            this.X = x;
+            this.Y = y;
+            this.Z = z;
+        }
+    }
+    public class Otrezki
+    {
+        public string point1;
+        public string point2;
+        public double distance;
+        public Otrezki(string point1,string point2,double distance)
+        {
+            this.point1 = point1;
+            this.point2 = point2;
+            this.distance = distance;
+        }
+    }
+
+ }
---
+++ LPR0_Derbin/Result.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace LPR0_Derbin
+{
+    /// <summary>
+    /// Логика взаимодействия для Result.xaml
+    /// </summary>
+    public partial class Result : Window
+    {
+        int k = 0;
+        public Result(List<Otrezki> otrezki)
+        {
+            InitializeComponent();
+            ShowResult(SortResult(otrezki));
+
+        }
+        public List<Otrezki> SortResult(List<Otrezki> otrezki)
+        {
+            var sort = from o in otrezki.OrderBy(o => o.distance)select o;
+            List<Otrezki> result = new List<Otrezki>();
+            foreach (var item in sort)
+            {
+                result.Add(item);
+            }
+            return result;
+        }
+        public void ShowResult(List<Otrezki> otrezki)
+        {
+            foreach (var item in otrezki)
+            {
+                if (k < 2)
+                {
+                    ResultRedTextBlock.Text += item.point1 + " - " + item.point2 + ": " + item.distance + "\n";
+                }
+                else
+                {
+                    ResultTextBlock.Text += item.point1 + " - " + item.point2 + ": " + item.distance + "\n";
+                }
+                
+                k++;
+            }
+            
+        }
+    }
+}
